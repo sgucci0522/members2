0db8e7cf932e4f16ab1debdfb88260eca5c3ae54
sgucci0522 <sgucci.0522@gmail.com>
2024-11-13 15:21:34 +0900

    バリデーション

--- members/resources/views/components/input-error.blade.php
+++ members/resources/views/components/input-error.blade.php
@@ -4,6 +4,10 @@
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
-        @endforeach
+	@endforeach
+
+	@if(empty($messages['image']))
+	    <li>画像ファイルがあれば、再度、選択してください。</li>
+	@endif
     </ul>
 @endif
--- members/resources/views/post/create.blade.php
+++ members/resources/views/post/create.blade.php
@@ -3,9 +3,11 @@
     <h2 class="font-semiblod text-gray-800 leading-tight">
        投稿の新規作成
     </h2>
-    @if(session('message'))
-	{{session('message')}}
-    @endif
+
+    <x-input-error class="mb-4" :messages="$errors->all()"/>
+
+    <x-message :message="session('message')"/>
+
   </x-slot>
 
 	 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -15,17 +17,17 @@
                     <div class="md:flex items-center mt-8">
                         <div class="w-full flex flex-col">
                         <label for="title" class="font-semibold leading-none mt-4">件名</label>
-                        <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" placeholder="Enter Title">
+                        <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{old('title')}}" placeholder="Enter Title">
                         </div>
                     </div>
     
                     <div class="w-full flex flex-col">
                         <label for="body" class="font-semibold leading-none mt-4">本文</label>
-                        <textarea name="body" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="body" cols="30" rows="10"></textarea>
+                        <textarea name="body" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="body" cols="30" rows="10">{{old('body')}}</textarea>
                     </div>
     
                     <div class="w-full flex flex-col">
-                        <label for="image" class="font-semibold leading-none mt-4">画像 </label>
+                        <label for="image" class="font-semibold leading-none mt-4">画像（１ＭＢまで） </label>
                         <div>
                         <input id="image" type="file" name="image">
                         </div>
