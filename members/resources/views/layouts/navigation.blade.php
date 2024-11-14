4aca3646ae3d708279fab3fe31cd89ea5bec4316
sgucci0522 <sgucci.0522@gmail.com>
2024-11-14 15:16:15 +0900

    メニューの追加

--- members/resources/views/layouts/navigation.blade.php
+++ members/resources/views/layouts/navigation.blade.php
@@ -12,8 +12,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <!-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+		    </x-nav-link> -->
+                    <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
+                        HOME
+                    </x-nav-link>
+                    <x-nav-link :href="route('post.create')" :active="request()->routeIs('post.create')">
+                        新規作成
                     </x-nav-link>
                 </div>
             </div>
@@ -67,10 +73,21 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
+                HOME
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('post.create')" :active="request()->routeIs('post.create')">
+                新規作成
+            </x-responsive-nav-link>
+        </div>
+
+    <!-- <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-        </div>
+	</div> -->
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
